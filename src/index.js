e02504be8541e7ba56d0365f77c3e9266cf785cb
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-08-25 20:02:58 +0200

    added seconds to JS

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ function updateTime() {
   let londonTimeElement = londonElement.querySelector(".time");
   let londonTime = moment().tz("Europe/London");
   londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonTime.format("h:mm [<small>]A[</small>]");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
   //paris
   let parisElement = document.querySelector("#paris");
   let parisDateElement = parisElement.querySelector(".date");
   let parisTimeElement = parisElement.querySelector(".time");
   let parisTime = moment().tz("Europe/Paris");
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm [<small>]A[</small>]");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
   //new york
   let newYorkElement = document.querySelector("#new-york");
   let newYorkDateElement = newYorkElement.querySelector(".date");
@@ -20,7 +22,7 @@ function updateTime() {
   let newYorkTime = moment().tz("America/New_York");
   newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
   newYorkTimeElement.innerHTML = newYorkTime.format(
-    "h:mm [<small>]A[</small>]"
+    "h:mm:ss [<small>]A[</small>]"
   );
 }
 updateTime();
