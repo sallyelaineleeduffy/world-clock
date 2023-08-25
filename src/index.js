ec4a81972c6c7c8cf58d834ce90cf93fb39a106d
Sally Lee-Duffy <sally@thesnowinstitute.com>
2023-08-25 19:59:48 +0200

    Add JS to get correct time and date info for static cities

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-
+function updateTime() {
+  //london
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
+  londonTimeElement.innerHTML = londonTime.format("h:mm [<small>]A[</small>]");
+  //paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm [<small>]A[</small>]");
+  //new york
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
+  newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
